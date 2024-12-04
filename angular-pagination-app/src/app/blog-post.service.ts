0aaf3d3f14798ffe997c50b5c056a7bd052c2888
Juliana Cortes <cortej19@matc.edu>
2024-12-04 13:51:49 -0500

    provide http client in app config

--- angular-pagination-app/src/app/blog-post.service.ts
+++ angular-pagination-app/src/app/blog-post.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BlogPostService {
+  private apiUrl = 'http://localhost:3000/api/posts';
+  http = inject(HttpClient);
+  constructor() {}
 
-  constructor() { }
+  getPosts(page: number, limit: number, searchQuery: string): Observable<any> {
+    const params = { page: page.toString(), limit: limit.toString(), searchQuery };
+    return this.http.get(this.apiUrl, { params });
+  }
 }
